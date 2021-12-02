e4d2276e5f3ec08e4757db485e8e75400337a2ce
Ignacio dos Santos Claro <idossantos@fi.uba.ar>
2021-12-01 21:01:28 -0300

    agrego Diccionario.h

---
+++ Diccionario.h
@@ -0,0 +1,22 @@
+#ifndef DICCIONARIO_H
+#define DICCIONARIO_H
+
+#include "ABB.h"
+
+using namespace std;
+
+template<class Dato>
+class Diccionario {
+
+    //Atributos
+
+    private:
+
+
+    //Metodos
+
+
+}
+
+
+#endif //DICCIONARIO_H
