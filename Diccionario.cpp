db0dffbb999809b93af22f53b3ca87d3905028b2
Juan Hirschmann <juanhirschmann@gmail.com>
2021-12-09 19:08:33 -0300

    Auto stash before merge of "main" and "origin/main"

--- Diccionario.cpp
+++ Diccionario.cpp
@@ -1,46 +1,55 @@
 #include "Diccionario.h"
 
-Diccionario::Diccionario() {
+Diccionario::Diccionario()
+{
     arbol = new ABB<Edificio>;
 }
 
-void Diccionario::alta(Edificio edificio) {
+void Diccionario::alta(Edificio edificio)
+{
     string nombre = edificio.obtener_nombre();
     arbol->insertar_nodo(nombre, edificio);
 }
 
-void Diccionario::baja(string nombre_edificio) {
+void Diccionario::baja(string nombre_edificio)
+{
     arbol->remover_nodo(nombre_edificio);
 }
 
-Edificio* Diccionario::consulta(string nombre_edificio) {
-    Nodo_ABB<Edificio>* edificio_buscado = arbol->buscar(nombre_edificio);
-    if(edificio_buscado == nullptr) {
+Edificio *Diccionario::consulta(string nombre_edificio)
+{
+    Nodo_ABB<Edificio> *edificio_buscado = arbol->buscar(nombre_edificio);
+    if (edificio_buscado == nullptr)
+    {
         return nullptr;
     }
     return arbol->buscar(nombre_edificio)->obtener_dato();
 }
-    
-void Diccionario::recorrer() {//imprime por pantalla el recorrido in order con las claves de cada nodo del ABB
+
+void Diccionario::recorrer()
+{ // imprime por pantalla el recorrido in order con las claves de cada nodo del ABB
     arbol->imprimir_in_order();
 }
 
-//void Diccionario::reemplazar(string viejo, Edificio* edificio) {
-//    Nodo_ABB<Edificio>* aux = arbol->buscar(viejo);
-//    aux->~Nodo_ABB(); //borro la memoria que ocupaba el dato viejo
-//    aux->setear_dato(edificio); //establezco el dato nuevo
-//    aux->setear_clave(edificio->obtener_nombre()); //establezco la clave nueva para el nodo
-//    arbol->buscar(viejo)->~Nodo_ABB();
-//    arbol->buscar(viejo)->setear_dato(edificio);
-//}
+// void Diccionario::reemplazar(string viejo, Edificio* edificio) {
+//     Nodo_ABB<Edificio>* aux = arbol->buscar(viejo);
+//     aux->~Nodo_ABB(); //borro la memoria que ocupaba el dato viejo
+//     aux->setear_dato(edificio); //establezco el dato nuevo
+//     aux->setear_clave(edificio->obtener_nombre()); //establezco la clave nueva para el nodo
+//     arbol->buscar(viejo)->~Nodo_ABB();
+//     arbol->buscar(viejo)->setear_dato(edificio);
+// }
 
-Vector<Edificio*> Diccionario::arbol_a_vector() {
-    Vector<Edificio*> vector;
+Vector<Edificio *> Diccionario::arbol_a_vector()
+{
+    Vector<Edificio *> vector;
     arbol->arbol_a_vector(vector);
     return vector;
 }
 
-Diccionario::~Diccionario() {
-    arbol->~ABB();
-    arbol = nullptr;
+Diccionario::~Diccionario()
+{
+    // arbol->~ABB();
+    delete arbol;
+    cout << "Mori diccionario" << endl;
 }
