2e740eb60f5dc66433df164b7e2fe998e40dbcf9
Juan Hirschmann <juanhirschmann@gmail.com>
2021-12-09 19:08:33 -0300

    Perdia memoria diccionario y jugador

--- Jugador.cpp
+++ Jugador.cpp
@@ -3,63 +3,80 @@
 
 using namespace std;
 
-Jugador::Jugador() {
+Jugador::Jugador()
+{
     this->energia = ENERGIA_INICIAL;
     this->edificios = new Diccionario;
 }
 
-int Jugador::obtener_energia() {
+int Jugador::obtener_energia()
+{
     return energia;
 }
 
-void Jugador::desplazarse(int pos_x, int pos_y) {
+void Jugador::desplazarse(int pos_x, int pos_y)
+{
     this->posicion.fijar_coordenadas(pos_x, pos_y);
 }
 
-void Jugador::modificar_energia(int energia) {
-    if(this->energia += energia >= ENERGIA_MAXIMA) {
+void Jugador::modificar_energia(int energia)
+{
+    if (this->energia += energia >= ENERGIA_MAXIMA)
+    {
         this->energia = ENERGIA_MAXIMA;
     }
-    else if(this->energia += energia <= ENERGIA_MINIMA) {
+    else if (this->energia += energia <= ENERGIA_MINIMA)
+    {
         this->energia = ENERGIA_MINIMA;
     }
-    else {
+    else
+    {
         this->energia += energia;
     }
 }
 
-void Jugador::agregar_consumible_inventario(string nombre, int cantidad, char icono) {
+void Jugador::agregar_consumible_inventario(string nombre, int cantidad, char icono)
+{
     this->inventario.agregar_material_consumible_al_final(nombre, cantidad, icono);
 }
 
-void Jugador::agregar_material_inventario(string nombre, int cantidad) {
+void Jugador::agregar_material_inventario(string nombre, int cantidad)
+{
     this->inventario.agregar_material_al_final(nombre, cantidad);
 }
 
-void Jugador::modificar_cantidad_bomba(int cantidad) {
+void Jugador::modificar_cantidad_bomba(int cantidad)
+{
     bombas.modificar_cantidad(cantidad);
 }
 
-void Jugador::mostrar_inventario() {
-    for(int i = 0; i < inventario.obtener_largo(); i++) {
+void Jugador::mostrar_inventario()
+{
+    for (int i = 0; i < inventario.obtener_largo(); i++)
+    {
         cout << inventario.obtener_valor(i)->obtener_nombre() << " " << inventario.obtener_valor(i)->obtener_cantidad() << endl;
     }
     cout << "Bombas: " << bombas.obtener_cantidad() << endl;
 }
 
-void Jugador::asignar_edificios(Diccionario* diccionario) {
+void Jugador::asignar_edificios(Diccionario *diccionario)
+{
     this->edificios = diccionario;
 }
 
-Diccionario* Jugador::obtener_edificios() {
+Diccionario *Jugador::obtener_edificios()
+{
     return this->edificios;
 }
 
-void Jugador::agregar_edificio(Edificio edificio) {
+void Jugador::agregar_edificio(Edificio edificio)
+{
     this->edificios->alta(edificio);
 }
 
-Jugador::~Jugador() {
-    this->edificios->~Diccionario();
+Jugador::~Jugador()
+{
+    delete this->edificios;
+    cout << "Mori jugador" << endl;
     this->edificios = nullptr;
 }
--- main.cpp
+++ main.cpp
@@ -5,16 +5,30 @@
 #include <iostream>
 #include "configuracion.h"
 #include "Objetivo_material.h"
-#include "Objetivo_edificio.h"
 #include "Material_consumible.h"
 #include "Grafo.h"
 #include "ABB.h"
+#include "Menu.h"
 #include "mecanicas_juego.h"
-#include "Objetivo_edificio_multiple.h"
-#include "Objetivo_edificio.h"
-#include "menu.h"
+
 int main(void)
 {
-    iniciar_juego();
+    Matriz_casillero mapa;
+    Grafo grafo1;
+    Grafo grafo2;
+    Jugador jugador1;
+    Jugador jugador2;
+    //    ABB<int> arbol;
+    Diccionario diccionario;
+
+    procesar_mapa(mapa);
+    cargar_materiales(jugador1, jugador2);
+    cout << "aca" << endl;
+    procesar_edificios(diccionario, jugador1, jugador2);
+    procesar_ubicaciones(mapa, jugador1, jugador2);
+    cout << "aca2" << endl;
+    listar_todos_edificios(diccionario);
+    mostrar_mapa(mapa, 5, 3);
+
     return 0;
-}
+};
